15e9dc21cb4d35f8480e5bbf240d3196ddcaf7c6
Prajeen Govardhanam <prajeenrg@gmail.com>
2022-12-08 00:14:50 +0530

    ported cpp code to go line by line to integrate bitwise cipher

---
+++ cipher/bitwise.go
@@ -0,0 +1,25 @@
+package bitwise
+
+import "strings"
+
+type Bitwise struct{}
+
+func (alg Bitwise) GetEncryptionKey() string {
+	return "0x0"
+}
+
+func (alg Bitwise) processText(txt string) string {
+	var str strings.Builder
+	for ch := range txt {
+		str.WriteRune(rune(^ch))
+	}
+	return str.String()
+}
+
+func (alg Bitwise) Encrypt(msg string) string {
+	return alg.processText(msg) + alg.GetEncryptionKey()
+}
+
+func (alg Bitwise) Decrypt(cip string) string {
+	return alg.processText(cip)
+}
