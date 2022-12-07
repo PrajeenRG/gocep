4e16f7139fd1407ac74735105d9266be8474e7b5
Prajeen Govardhanam <prajeenrg@gmail.com>
2022-12-08 00:20:08 +0530

    ported cpp code to go line by line to integrate xor cipher

---
+++ cipher/xor.go
@@ -0,0 +1,27 @@
+package xor
+
+import "strings"
+
+type Xor struct {
+	key int
+}
+
+func (alg Xor) GetEncryptionKey() string {
+	return "6x6"
+}
+
+func (alg Xor) processText(txt string) string {
+	var str strings.Builder
+	for ch := range txt {
+		str.WriteRune(rune(alg.key ^ ch))
+	}
+	return str.String()
+}
+
+func (alg Xor) Encrypt(msg string) string {
+	return alg.processText(msg) + alg.GetEncryptionKey()
+}
+
+func (alg Xor) Decrypt(cip string) string {
+	return alg.processText(cip)
+}
