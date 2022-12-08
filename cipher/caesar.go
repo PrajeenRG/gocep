e0fa3c5d11c7a02d14a0284236f01f5cec8056c5
Prajeen Govardhanam <prajeenrg@gmail.com>
2022-12-08 08:28:59 +0530

    ported cpp code to go line by line to integrate caesar cipher

---
+++ cipher/caesar.go
@@ -0,0 +1,25 @@
+package caesar
+
+import "strings"
+
+type Caesar struct{}
+
+func (alg Caesar) GetEncryptionKey() string {
+	return "2x2"
+}
+
+func (alg Caesar) Encrypt(msg string) string {
+	var str strings.Builder
+	for _, ch := range msg {
+		str.WriteRune(ch + 5)
+	}
+	return str.String()
+}
+
+func (alg Caesar) Decrypt(cip string) string {
+	var str strings.Builder
+	for _, ch := range cip {
+		str.WriteRune(ch - 5)
+	}
+	return str.String()
+}
